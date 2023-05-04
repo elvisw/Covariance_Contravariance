7afe009ff2f13d53f1ed6087a7c1175edff11586
elvis_w <me@elvisw.com>
2023-05-04 14:14:03 +0800

    简化语法

--- Covariance_Contravariance/农产品.cs
+++ Covariance_Contravariance/农产品.cs
@@ -10,10 +10,7 @@ namespace Covariance_Contravariance
     {
         public float 数量 { get; set; }
         public decimal 单价 { get; set; }
-        public decimal 计算总价()
-        {
-            return 单价 * (decimal)数量;
-        }
+        public decimal 总价 => 单价 * (decimal)数量;
     }
 
     class 豆类 : 农产品
@@ -40,12 +37,12 @@ namespace Covariance_Contravariance
     class 生产者类<T> : I农产品生产者<T> where T : 农产品, new()
     {
         private T? _产品;
-        public void 生产产品(float 生产数量, decimal 生产单价)
+        public void 生产产品(float 生产数量, decimal 成本)
         {
             _产品 = new T
             {
                 数量 = 生产数量,
-                单价 = 生产单价
+                单价 = 成本
             };
             Console.WriteLine($"完成商品生产：数量：{_产品.数量}，单价：{_产品.单价}。");
 
@@ -60,9 +57,9 @@ namespace Covariance_Contravariance
                 return 产品打包;
             }
             else
-            {  
+            {
                 throw new InvalidOperationException("没有生产产品，工厂无库存");
-            }            
+            }
         }
     }
     interface I农产品消费者<in T> where T : 农产品  //T是逆变的
